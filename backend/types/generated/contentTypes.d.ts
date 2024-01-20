32c8808eebd9129d42423c18032d1b3e3ab3aab5
Petr Losev <geraltrivski@mail.ru>
2024-01-21 00:52:21 +0300

    Backend request tuned.

--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -362,6 +362,36 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiGreetingGreeting extends Schema.SingleType {
+  collectionName: 'greetings';
+  info: {
+    singularName: 'greeting';
+    pluralName: 'greetings';
+    displayName: 'greeting';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    greeting: Attribute.String;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::greeting.greeting',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::greeting.greeting',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -778,6 +808,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::greeting.greeting': ApiGreetingGreeting;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
--- frontend/app.vue
+++ frontend/app.vue
@@ -1,7 +1,27 @@
 <template>
-  <div> {{ data }} </div>
+  <div> {{ greet }} </div>
 </template>
 
 <script setup>
-  let { data } = await useFetch('/api/foo')
+const url =  'http://localhost:1337/api/greeting';
+
+let greet = ref('')
+
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    greet.value =  data.data.attributes.greeting
+  })
+  .catch(error => {
+    console.error('Error:', error.message);
+  });
+
+onMounted(() => {
+  fetch(url);
+})
 </script>
